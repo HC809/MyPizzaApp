ed1c8b14b2e154ba8453cd28e69826c9b777952f
Hector Caballero <caballero.22.809@gmail.com>
2024-05-25 16:44:23 -0600

    Add Product Test Create_Should_SetPropertyValue

---
+++ AlbaPizza.Domain.UnitTests/Products/ProductData.cs
@@ -0,0 +1,9 @@
+﻿using AlbaPizzaApp.Domain.Products;
+
+namespace AlbaPizza.Domain.UnitTests.Products;
+internal static class ProductData
+{
+    public static readonly string Description = "Pizza Suprema";
+    public static readonly decimal Price= 350;
+    public static readonly ProductTaxType TaxType = ProductTaxType.General;
+}
---
+++ AlbaPizza.Domain.UnitTests/Products/ProductTests.cs
@@ -0,0 +1,18 @@
+﻿using AlbaPizzaApp.Domain.Products;
+using FluentAssertions;
+
+namespace AlbaPizza.Domain.UnitTests.Products;
+public class ProductTests 
+{
+    [Fact]
+    public void Create_Should_SetPropertyValue()
+    {
+        // Act
+        var product = Product.Create(ProductData.Description, ProductData.Price, ProductData.TaxType);
+
+        // Assert
+        product.Description.Should().Be(ProductData.Description);
+        product.Price.Should().Be(ProductData.Price);
+        product.TaxType.Should().Be(ProductData.TaxType);
+    }
+}
