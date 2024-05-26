e75f3c864d1c66500b35ebfaf977202d530586e3
Hector Caballero <caballero.22.809@gmail.com>
2024-05-26 11:01:41 -0600

    Add Exceptions and ValidationBehavior

---
+++ AlbaPizzaApp.Aplication/Exceptions/ResultException.cs
@@ -0,0 +1,12 @@
+﻿using AlbaPizzaApp.Domain.Abstractions;
+
+namespace AlbaPizzaApp.Application.Exceptions;
+public class ResultException : Exception
+{
+    public Error Error { get; }
+
+    public ResultException(Error error) : base(error.Description)
+    {
+        Error = error;
+    }
+}
---
+++ AlbaPizzaApp.Aplication/Exceptions/ValidationError.cs
@@ -0,0 +1,2 @@
+﻿namespace AlbaPizzaApp.Application.Exceptions;
+public sealed record ValidationError(string PropertyName, string ErrorMessage);
