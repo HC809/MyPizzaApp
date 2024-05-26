db3faa315ce9bceb65065694d157f088b6b74645
Hector Caballero <caballero.22.809@gmail.com>
2024-05-26 10:25:13 -0600

    Add Customer Tests

---
+++ AlbaPizza.Domain.UnitTests/Customers/CustomerData.cs
@@ -0,0 +1,9 @@
+﻿using AlbaPizzaApp.Domain.Products;
+
+namespace AlbaPizza.Domain.UnitTests.Customers;
+internal static class CustomerData
+{
+    public static readonly string Name = "Hector Caballero";
+    public static readonly string Email = "caballero.22.809@gmail.com";
+    public static readonly string Phone = "31907137";
+}
---
+++ AlbaPizza.Domain.UnitTests/Customers/CustomerTests.cs
@@ -0,0 +1,18 @@
+﻿using AlbaPizzaApp.Domain.Customers;
+using FluentAssertions;
+
+namespace AlbaPizza.Domain.UnitTests.Customers;
+public class CustomerTests
+{
+    [Fact]
+    public void Create_Should_SetPropertyValue()
+    {
+        // Act
+        var customer = Customer.Create(CustomerData.Name, CustomerData.Email, CustomerData.Phone);
+
+        // Assert
+        customer.Name.Should().Be(CustomerData.Name);
+        customer.Email.Should().Be(CustomerData.Email);
+        customer.Phone.Should().Be(CustomerData.Phone);
+    }
+}
