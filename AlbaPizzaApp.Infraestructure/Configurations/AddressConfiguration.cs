3a2078c1c684ed96074d47c2e3e3986321f3bde6
Hector Caballero <caballero.22.809@gmail.com>
2024-05-25 21:27:51 -0600

    Add Customer & Address configurations

---
+++ AlbaPizzaApp.Infraestructure/Configurations/AddressConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using AlbaPizzaApp.Domain.Addresses;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using AlbaPizzaApp.Domain.Customers;
+
+namespace AlbaPizzaApp.Infraestructure.Configurations;
+internal sealed class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.ToTable("Addresses");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Country)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.State)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.City)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Street)
+            .IsRequired()
+            .HasMaxLength(250);
+
+        builder.Property(x => x.ZipCode)
+            .IsRequired()
+            .HasMaxLength(20);
+
+        builder.Property(x => x.CustomerId).IsRequired();
+
+        builder.HasOne<Customer>()
+            .WithMany()
+            .HasForeignKey(x => x.CustomerId);
+    }
+}
+
---
+++ AlbaPizzaApp.Infraestructure/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using AlbaPizzaApp.Domain.Addresses;
+using AlbaPizzaApp.Domain.Customers;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlbaPizzaApp.Infraestructure.Configurations;
+internal sealed class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.ToTable("Customers");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Phone)
+            .IsRequired()
+            .HasMaxLength(8);
+    }
+}
+
