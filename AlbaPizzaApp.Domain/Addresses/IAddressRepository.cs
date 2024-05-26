37a2a4377cdf1e7a4f5e5979b4ca2a660bb6f64c
Hector Caballero <caballero.22.809@gmail.com>
2024-05-26 07:29:11 -0600

    Add AddressRepository

---
+++ AlbaPizzaApp.Domain/Addresses/IAddressRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace AlbaPizzaApp.Domain.Addresses;
+public interface IAddressRepository
+{
+    Task<Address?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    void Add(Address product);
+    void Update(Address product);
+    void Remove(Address product);
+}
---
+++ AlbaPizzaApp.Infraestructure/Repositories/AddressRepository.cs
@@ -0,0 +1,7 @@
+﻿using AlbaPizzaApp.Domain.Addresses;
+
+namespace AlbaPizzaApp.Infraestructure.Repositories;
+internal sealed class AddressRepository : Repository<Address>, IAddressRepository
+{
+    public AddressRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+}
